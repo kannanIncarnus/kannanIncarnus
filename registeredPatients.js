214068fabcea42a1419a94b1915b79f1bbcd1817
IncarnusTech <kannan26n@gmail.com>
2019-07-11 18:56:36 +0530

    datatype int

--- registeredPatients.js
+++ registeredPatients.js
@@ -84,7 +84,7 @@
             }, {
                 id: "age",
                 alias: "age",
-                dataType: tableau.dataTypeEnum.string
+                dataType: tableau.dataTypeEnum.int
             }, {
                 id: "mrn",
                 alias: "mrn",
