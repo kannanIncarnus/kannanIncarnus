b4e6d5e571fce8cfcf789640782874a20b04a06c
IncarnusTech <kannan26n@gmail.com>
2019-07-15 18:49:43 +0530

    fixes

--- testpagination.js
+++ testpagination.js
@@ -23,7 +23,6 @@
     myConnector.getData = function (table, doneCallback) {
         $.getJSON("https://demo.incarnus.com:8850/thirdparty/tableauservice/patientreports/getregisteredpatients/2019-06-01/2019-07-04", function (resp) {
             var data = resp.registeredpatients,
-                totalRecords = tableData.count
 
                 // Iterate over the JSON object
                 for (var i = 0, len = data.length; i < len; i++) {
