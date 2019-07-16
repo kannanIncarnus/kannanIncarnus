240afd0bf21dc6abb2ebbc79ff1624bd9ecb07ab
IncarnusTech <kannan26n@gmail.com>
2019-07-16 15:32:47 +0530

    fixes

--- testpagination.js
+++ testpagination.js
@@ -25,9 +25,6 @@
 
     myConnector.getData = function (table, doneCallback) {
 
-        var createdat = table.incrementValue
-        console.log("createdat: " + createdat);
-
         $.getJSON("https://demo.incarnus.com:8850/thirdparty/tableauservice/patientreports/getpatientswithpagination/" + limit + "/" + pagenumber, function (resp) {
             var data = resp.registeredpatients;
             let totalrecords = resp.totalrecords;
