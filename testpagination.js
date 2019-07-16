455e946c7f694619642303a28426ed5c200e0d37
IncarnusTech <kannan26n@gmail.com>
2019-07-16 16:11:33 +0530

    Update testpagination.js

--- testpagination.js
+++ testpagination.js
@@ -70,7 +70,7 @@
                 else {
                     table.appendRows(tableData);
                     console.log("Completed");
-                    console.log("Records: " + tableData.count);
+                    console.log("Records: " + tableData.length);
                     doneCallback();
                 }
             });
