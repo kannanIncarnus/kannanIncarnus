c02aea5259c485660250823081bace9b466d8936
IncarnusTech <kannan26n@gmail.com>
2019-07-17 11:20:57 +0530

    fixes

--- registeredPatients.js
+++ registeredPatients.js
@@ -18,7 +18,7 @@
         if (!modifiedat) {
             modifiedat = "2000-01-01"
         }
-        var queryPath = "https://demo.incarnus.com:8850/thirdparty/tableauservice/patientreports/getpatientsdata/" + limit + "/" + pagenumber + "/" + modifiedat
+        var queryPath = "https://demo.incarnus.com:8850/thirdparty/tableauservice/getregisteredpatientsdata/getpatientsdata/" + limit + "/" + pagenumber + "/" + modifiedat
 
         $.getJSON(queryPath, function (resp) {
             var data = resp.registeredpatients;
