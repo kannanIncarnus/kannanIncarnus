34180c49965ad8a2052d789b32d92494348bbd8f
IncarnusTech <kannan26n@gmail.com>
2019-07-18 18:17:44 +0530

    fixes

--- registeredPatients.js
+++ registeredPatients.js
@@ -17,9 +17,6 @@
         if (!modifiedat) {
             modifiedat = "2000-01-01"
         }
-        else {
-            tableData = [];
-        }
 
         console.log("modifiedat: " + modifiedat);
         var queryPath = "https://demo.incarnus.com:8850/thirdparty/tableauservice/patientreports/getregisteredpatientsdata/" + limit + "/" + pagenumber + "/" + modifiedat
@@ -31,8 +28,9 @@
             console.log("totalRecords in the collection: " + totalrecords);
                 // Iterate over the JSON object
                 for (var i = 0, len = data.length; i < len; i++) {
+                    let serialno = (limit * (pagenumber-1)) + i;
                     tableData.push({
-                        "SNo": String((limit * (pagenumber-1))+i),
+                        "SNo": String(serialno+1),
                         "regid": data[i].regid,
                         "id": data[i]._id,
                         "PatientName": data[i].firstname,
@@ -58,17 +56,17 @@
                     });
                 }
         
-                if ((limit * pagenumber) < totalrecords) {
-                    console.log("Fetching Again");
-                    console.log("totalRecords: " + (limit * pagenumber));
+                let currentrecords = (limit * pagenumber);
+                if (currentrecords < totalrecords) {
+                    console.log("Fetching Again with currentrecords: " + currentrecords);
                     pagenumber++;
                     myConnector.getData(table, doneCallback);
                 }
                 else {
                     pagenumber = 1;
                     table.appendRows(tableData);
-                    console.log("Completed");
-                    console.log("Fetched Records: " + tableData.length);
+                    console.log("CompletedRecords: " + tableData.length);
+                    tableData = [];
                     doneCallback();
                 }
             });
